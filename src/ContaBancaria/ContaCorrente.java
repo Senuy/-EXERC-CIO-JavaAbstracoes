1433b488146b3fe02d6757e2bb5b431806b5861c
Yunes <yunessilva_santos@hotmail.com>
2025-02-14 15:05:16 -0400

    Programa Conta corrente finalizado

--- src/ContaBancaria/ContaCorrente.java
+++ src/ContaBancaria/ContaCorrente.java
@@ -1,81 +1,86 @@
 package ContaBancaria;
 
 public class ContaCorrente {
-	
+
 	private double saldo;
-	
+
 	private final double chequeEspecial;
-	
+
 	private double cheque = 0.0;
-	
-	
-	//constructor
+
+	private double valor;
+
+	// constructor
 	public ContaCorrente(double saldo) {
 		this.saldo = saldo;
-		if(saldo<= 500) {
+		if (saldo <= 500) {
 			this.chequeEspecial = 50.00;
-		}else{
+		} else {
 			this.chequeEspecial = saldo * 0.5;
 		}
 	}
-	//get and set
+
+	// get and set
 	public double getSaldo() {
 		return saldo;
 	}
 
-	public void deposito (double saldo) {
+	public void deposito(double saldo) {
 		this.saldo += saldo;
 	}
-	
+
 	public void sacarSaldo(double valor) {
-		if(saldo > valor) {
-			
-			this.saldo -=valor;
-			
-		}else if(saldo <= valor && (saldo + chequeEspecial) >= valor){
-			
-			cheque = (this.saldo - valor)*0.2;
-			
-			if(cheque != 0){
-			this.saldo = this.saldo - valor - cheque;
+		if (saldo > valor) {
+
+			this.saldo -= valor;
+
+		} else if (saldo <= valor && (saldo + chequeEspecial) >= valor) {
+
+			this.valor = valor;
+			cheque = (valor - this.saldo) * 0.2;
+
+			if (cheque != 0) {
+				this.saldo = this.saldo - valor - cheque;
+			} else {
+				this.saldo = this.saldo - valor;
 			}
-			this.saldo = this.saldo - valor;
-			// implementar o restante de cheque especial 
-			
+
+			// implementar o restante de cheque especial
+
 		} else {
 			System.out.println("Valor de Saldo + Cheque especial  insuficientes");
 		}
-		
+
 	}
+
 	public void pagarBoleto(double valorb) {
-		/*if(saldo > valor) {
-			
-			this.saldo -=valor;
-			
-		}else if(saldo < valor && (saldo + chequeEspecial) >= valor){
-			
-			double cheque = (this.saldo - valor)*0.2;
-			this.saldo = this.saldo - (this.saldo - valor) - cheque;
-			// implementar o restante de cheque especial 
-			
-		} else {
-			System.out.println("Valor de Saldo + Cheque especial para o pagamento do boleto  insuficientes");
-		}*/
-		
+		/*
+		 * if(saldo > valor) {
+		 * 
+		 * this.saldo -=valor;
+		 * 
+		 * }else if(saldo < valor && (saldo + chequeEspecial) >= valor){
+		 * 
+		 * double cheque = (this.saldo - valor)*0.2; this.saldo = this.saldo -
+		 * (this.saldo - valor) - cheque; // implementar o restante de cheque especial
+		 * 
+		 * } else { System.out.
+		 * println("Valor de Saldo + Cheque especial para o pagamento do boleto  insuficientes"
+		 * ); }
+		 */
+
 		sacarSaldo(valorb);
-		
-	}
-	public void usaCheque() {
-		if(saldo < 0) {
-			System.out.println("O uso do cheque está em R$"+ cheque);
-		}
+
 	}
 
 	public double getChequeEspecial() {
 		return chequeEspecial;
 	}
 
-	
-	
+	public void usaCheque() {
+		if (saldo < 0) {
+			System.out.println("O uso do cheque está em R$" + valor + " mais juro de 20%, R$" + cheque);
+		}
 
+	}
 }
--- src/ContaBancaria/Main.java
+++ src/ContaBancaria/Main.java
@@ -1,27 +1,127 @@
 package ContaBancaria;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		ContaCorrente conta = new ContaCorrente(600.00);
-		ContaCorrente conta2 = new ContaCorrente(1500.00);
-				
-		System.out.println(conta.getSaldo());
-		System.out.println(conta.getChequeEspecial());
-		
-		//conta.deposito(500.00);
-		
-		//System.out.println(conta.getSaldo());
-		
-		conta.sacarSaldo(600.00);
-	
-		System.out.println(conta.getSaldo());
-		
-		conta.pagarBoleto(250);
 
-		System.out.println(conta.getSaldo());
-		
-		conta.usaCheque();
+		Scanner scanner = new Scanner(System.in);
+
+		ContaCorrente conta = null;
+		/*
+		 * ContaCorrente conta = new ContaCorrente(600.00); ContaCorrente conta2 = new
+		 * ContaCorrente(1500.00);
+		 * 
+		 * System.out.println(conta.getSaldo());
+		 * System.out.println(conta.getChequeEspecial());
+		 * 
+		 * //conta.deposito(500.00);
+		 * 
+		 * //System.out.println(conta.getSaldo());
+		 * 
+		 * conta.sacarSaldo(600.00);
+		 * 
+		 * System.out.println(conta.getSaldo());
+		 * 
+		 * conta.pagarBoleto(250);
+		 * 
+		 * System.out.println(conta.getSaldo());
+		 * 
+		 * conta.usaCheque();
+		 */
+
+		while (true) {
+			System.out.println("\n === Menu ===");
+			System.out.println("1 - Criar Conta");
+			System.out.println("2 - Consultar  Saldo");
+			System.out.println("3 - Consultar Cheque Especial");
+			System.out.println("4 - Depositar");
+			System.out.println("5 - Sacar");
+			System.out.println("6 - Pagar Boleto");
+			System.out.println("7 - Está usando Cheque especial?");
+			System.out.println("0 - SAIR");
+			System.out.println("Escolha uma opcao: ");
+
+			int opcao = scanner.nextInt();
+
+			switch (opcao) {
+			case 1:
+				if (conta == null) {
+					System.out.println("Digite o saldo inicial: ");
+					double saldo = scanner.nextDouble();
+					conta = new ContaCorrente(saldo);
+				} else {
+					System.out.println("Conta já criada");
+				}
+				break;
+
+			// ----------------------------------------------------------
+			case 2:
+				if (conta != null) {
+					System.out.println("Saldo da Conta R$" + conta.getSaldo());
+				} else {
+					System.out.println("Criei uma conta primeiro");
+				}
+				break;
+			// ----------------------------------------------------------
+			case 3:
+				if (conta != null) {
+					System.out.println("Saldo Cheque Especial R$" + conta.getChequeEspecial());
+				} else {
+					System.out.println("Criei uma conta primeiro");
+				}
+				break;
+			// ----------------------------------------------------------
+			case 4:
+				if (conta != null) {
+					System.out.println("Insira um valor a depositar R$");
+					double deposito = scanner.nextDouble();
+					conta.deposito(deposito);
+				} else {
+					System.out.println("Criei uma conta primeiro");
+				}
+				break;
+			// ----------------------------------------------------------
+			case 5:
+				if (conta != null) {
+					System.out.print("Digite um valor a Sacar R$");
+					double saque = scanner.nextDouble();
+					conta.sacarSaldo(saque);
+
+				} else {
+					System.out.println("Criei uma conta primeiro");
+				}
+				break;
+			// ----------------------------------------------------------
+
+			case 6:
+				if (conta != null) {
+					System.out.print("Insira o valor do boleto R$");
+					double saque = scanner.nextDouble();
+					conta.pagarBoleto(saque);
+
+				} else {
+					System.out.println("Criei uma conta primeiro");
+				}
+				break;
+			// ----------------------------------------------------------
+			case 7:
+				if (conta != null) {
+					conta.usaCheque();
+				} else {
+					System.out.println("Criei uma conta primeiro");
+				}
+				break;
+			// ----------------------------------------------------------
+			case 0:
+				System.out.println("Saindo");
+				scanner.close();
+				return;
+			default:
+				System.out.println("Opção inválida");
+			}
+		}
 	}
 
 }
